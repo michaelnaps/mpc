53a6516681ccafe20df1f1d45396a8b1642d41d9
Michael Napoli <mikednapoli@gmail.com>
2023-01-11 21:00:34 -0500

    pendulum model working with small changes to mpc: can add discrete model options when ready

--- examples/pendulum.py
+++ examples/pendulum.py
@@ -1,7 +1,9 @@
 import sys
 sys.path.insert(0, '../.');
 
-import numpy as np
+import math
+import matplotlib.pyplot as plt
+
 from mpc import *
 
 def model(q, u, _):
@@ -12,23 +14,44 @@ def model(q, u, _):
 
     dq = [
         q[1],
-        u[0] - g/l*np.cos(q[0]) - c*q[1]
+        u[0] - g/l*math.cos(q[0]) - c*q[1]
     ];
 
     return dq;
 
-def cost(mpc_var, q, u, params):
+def cost(mpc_var, qlist, ulist, params):
     qd = [np.pi/2, 0];
-    C = (q[2][0] - qd[0])**2
+    kq = 1;
+    kdq = 0.1;
+    ku = 0;
+
+
+    C = 0;
+    for i in range(mpc_var.PH):
+        C += kq*(qlist[i][0] - qd[0])**2;
+        C += kdq*(qlist[i][1] - qd[1])**2;
+        C += ku*ulist[i]**2;
+    C += kq*(qlist[-1][0] - qd[0])**2 + kdq*(qlist[-1][1] - qd[1])**2;
+
     return C;
 
+def pendulum(T, q):
+    fig, axes = plt.subplots();
+
+    axes.plot(T, q);
+
 if __name__ == "__main__":
-    PH_length = 5;
-    mpc_var = ModelPredictiveControl('nno', cost, model, 'no params', 1, 1, PH_length);
+    params = None;
+    PH_length = 10;
+
+    mpc_var = ModelPredictiveControl('nno', cost, model, params, 1, 1, PH_length);
+    mpc_var.setMinTimeStep(1);
 
     q0 = [0, 0];
     uinit = [0 for i in range(PH_length)];
 
-    uList = mpc_var.solve(q0, uinit, output=1);
+    sim_time = 10;
+    T, qlist, ulist = mpc_var.sim_root(sim_time, q0, uinit, output=1)[0:3];
 
-    print(mpc_var.simulate(q0, uList));
+    pendulum(T, qlist);
+    plt.show();
