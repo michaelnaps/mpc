6d28f1de11cc16966de6c6a8450252057dcf2ab8
Michael Napoli <mikednapoli@gmail.com>
2023-01-31 19:03:41 -0500

    callback a little more consistent, but still slow, attempt with patches

--- examples/Lorenz/lorenz.py
+++ examples/Lorenz/lorenz.py
@@ -38,19 +38,24 @@ def cost(mpc_var, qlist, ulist):
     return 0;
 
 def plot(T, q, params=None):
+    params.axs.set_xlim3d(-20, 20);
+    params.axs.set_ylim3d(-25, 25);
+    params.axs.set_zlim3d(0, 50);
     params.fig.tight_layout();
-    # params.axs.set_aspect('equal');
+
+    if q is not None:
+        npq = np.array(q);
+        params.axs.plot(npq[:,0], npq[:,1], npq[:,2]);
+        plt.show();
+
     return params;
 
 def callback(mpc_var, t, q, ulist):
-
     mpc_var.params.axs.plot([q[0]], [q[1]], [q[2]],
         color='b', marker='.', markersize=2);
-
-    mpc_var.params.axs.set_title("time: %.3f" % t)
+    mpc_var.params.axs.set_title("time: %.3f" % t);
 
     plt.show(block=0);
-
     plt.pause(mpc_var.params.pause);
 
     return Parameters(mpc_var.params.fig, mpc_var.params.axs);
@@ -64,15 +69,15 @@ if __name__ == "__main__":
     num_inputs = 3;
     PH_length = 10;
     num_ssvar = 3;
-    time_step = 0.001;
+    time_step = 0.01;
     model_type = 'continuous';
 
     mpc_var = ModelPredictiveControl('nno', model, cost, params, num_inputs,
         num_ssvar=num_ssvar, PH_length=PH_length, knot_length=1,
-        time_step=0.001, model_type=model_type);
+        time_step=time_step, model_type=model_type);
 
     # state initialization
-    q0 = [1, 1, 1.0000001];
+    q0 = [1, 1, 1];
     uinit = [0 for i in range(num_inputs*PH_length)];
 
     # simulate lorenz
@@ -80,5 +85,5 @@ if __name__ == "__main__":
     T, qlist, ulist = mpc_var.sim_root(sim_time, q0, uinit,
         callback=callback, output=0)[0:3];
 
-    # plot(T, qlist);
+    # plot(T, qlist, params);
     # plt.show();
