40031ec1fe8d38be09487419e7e50c76742e4332
Michael Napoli <mikednapoli@gmail.com>
2023-02-01 18:42:10 -0500

    using patches greatly improved performance

--- examples/Lorenz/lorenz.py
+++ examples/Lorenz/lorenz.py
@@ -5,7 +5,9 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as plt3d
-import matplotlib.patches as pat
+import matplotlib.patches as patch
+import matplotlib.path as path
+
 
 from mpc import *
 
@@ -61,45 +63,66 @@ def plot(T, q, params=None):
         npq = np.array(q);
         params.axs.plot(npq[:,0], npq[:,1], npq[:,2]);
         plt.show();
-    else:
+    else:  # if T=None then q=q0
+        q0list = np.kron(np.ones( (25,1) ), q);
 
+        params.trail = (patch.PathPatch(path.Path(q0list[:,0:2])), q0list[:,2], q0list);
+        params.axs.add_patch(params.trail[0]);
+        plt3d.art3d.pathpatch_2d_to_3d(params.trail[0], z=params.trail[1]);
+
+        params.event = q;
 
     return params;
 
 def callback(mpc_var, t, q, ulist):
-    mpc_var.params.axs.plot([q[0]], [q[1]], [q[2]],
-        color='b', marker='.', markersize=2);
+    # mpc_var.params.axs.plot([q[0]], [q[1]], [q[2]],
+    #     color='b', marker='.', markersize=2);
     mpc_var.params.axs.set_title("time: %.3f" % t);
+
+    mpc_var.params.trail[0].remove();
+
+    trailxyz = mpc_var.params.trail[2];
+    trailxyz[:-1] = trailxyz[1:];  trailxyz[-1] = q;
+
+    mpc_var.params.trail = (
+        patch.PathPatch(path.Path(trailxyz[:,:2])), trailxyz[:,2], trailxyz
+    );
+
+    mpc_var.params.axs.add_patch(mpc_var.params.trail[0]);
+    plt3d.art3d.pathpatch_2d_to_3d(mpc_var.params.trail[0], z=mpc_var.params.trail[1]);
 
     plt.show(block=0);
     plt.pause(mpc_var.params.pause);
 
-    return Parameters(mpc_var.params.fig, mpc_var.params.axs);
+    return Parameters(
+        mpc_var.params.fig, mpc_var.params.axs, mpc_var.params.trail
+    );
 
 
 if __name__ == "__main__":
+    # state initialization
+    q0 = [1, 1, 1];
+
     # MPC class variables
     params = Parameters();
-    params = plot(None,None,params);
+    params = plot(None, q0, params);
 
     num_inputs = 3;
     PH_length = 10;
     num_ssvar = 3;
-    time_step = 0.01;
+    time_step = 0.05;
     model_type = 'continuous';
 
     mpc_var = ModelPredictiveControl('nno', model, cost, params, num_inputs,
         num_ssvar=num_ssvar, PH_length=PH_length, knot_length=1,
         time_step=time_step, model_type=model_type);
 
-    # state initialization
-    q0 = [1, 1, 1];
+    # simulate lorenz
     uinit = [0 for i in range(num_inputs*PH_length)];
 
-    # simulate lorenz
     sim_time = 30;
     T, qlist, ulist = mpc_var.sim_root(sim_time, q0, uinit,
         callback=callback, output=0)[0:3];
 
     # plot(T, qlist, params);
-    # plt.show();
+    plt.show();
