84c82e9a7bbb823417ab0ccf3541d210b5f00865
Michael Napoli <mikednapoli@gmail.com>
2023-02-19 17:09:22 -0500

    using updating callback from ode class

--- Examples/SphereWorld/sphereworld.py
+++ Examples/SphereWorld/sphereworld.py
@@ -4,16 +4,57 @@ sys.path.insert(0, '/home/michaelnaps/prog/mpc');
 from mpc import *
 from Sphere import *
 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
+import matplotlib.path as path
+
+
 class Parameters:
-    def __init__(self, fig=None, axes=None):
-        if fig is None or axes is None:
-            self.fig, self.axes = plt.subplots();
+    def __init__(self, x0,
+                 fig=None, axs=None,
+                 buffer_length=10, pause=1e-3,
+                 color='k'):
+
+        if axs is None and fig is None:
+            self.fig, self.axs = plt.subplots();
         else:
             self.fig = fig;
-            self.axes = axes;
+            self.axs = axs;
+
+        self.axs.set_xlim(-10,10);
+        self.axs.set_ylim(-10,10);
+        self.axs.axis('equal');
+
+        # initialize buffer (trail)
+        self.color = color;
+
+        self.buffer = np.kron( np.ones( (buffer_length, 1) ), x0[:][:2]);
+        self.trail_patch = patch.PathPatch(path.Path(self.buffer), color=self.color);
+
+        self.axs.add_patch(self.trail_patch);
+
+        self.pause = pause;
+
+        # system specific
+        self.xd = [5, 7, 0, 0];
         self.sphereworld = init_sphereworld();
-        self.qd = [4, 8, 0, 0];
-        self.pause = 1e-6;
+
+        for sphere in self.sphereworld:
+            sphere.plot(self.axs);
+
+    def update(self, t, x):
+        self.trail_patch.remove();
+
+        self.buffer[:-1] = self.buffer[1:];
+        self.buffer[-1] = x[:2];
+
+        self.trail_patch = patch.PathPatch(path.Path(self.buffer), fill=0);
+        self.axs.add_patch(self.trail_patch);
+
+        plt.show(block=0);
+        plt.pause(self.pause);
+
+        return self;
 
 def init_sphereworld():
     wall = Sphere([0,0], -10);
@@ -24,74 +65,50 @@ def init_sphereworld():
 
     return (wall, obs1, obs2, obs3);
 
-def model(q, u, _):
+
+def model(x, u, _):
     dt = 0.025;
     c = 0.1;
 
     # discrete steps
-    qn = [
-        q[0] + dt*q[2],
-        q[1] + dt*q[3],
-        q[2] + dt*u[0] - c*q[2],
-        q[3] + dt*u[1] - c*q[3]
+    xn = [
+        x[0] + dt*x[2],
+        x[1] + dt*x[3],
+        x[2] + dt*u[0] - c*x[2],
+        x[3] + dt*u[1] - c*x[3]
     ];
 
-    return qn;
+    return xn;
 
-def cost(mpc_var, qlist, ulist):
+def cost(mpc_var, xlist, ulist):
     sphereworld = mpc_var.params.sphereworld;
-    qd = mpc_var.params.qd;
+    xd = mpc_var.params.xd;
     Nu = mpc_var.u_num;
     PH = mpc_var.PH;
 
-    kq = 1;
-    kdq = 0.1;
+    kx = 1;
+    kdx = 0.1;
     ku = 0.001;
     ko = 1;
 
     C = 0;
     k = 0;
     for i in range(PH):
-        C += kq*((qlist[i][0] - qd[0])**2 + (qlist[i][1] - qd[1])**2);
-        C += kdq*(qlist[i][2]**2 + qlist[i][3]**2);
+        C += kx*((xlist[i][0] - xd[0])**2 + (xlist[i][1] - xd[1])**2);
+        C += kdx*(xlist[i][2]**2 + xlist[i][3]**2);
         C += ku*(ulist[k]**2 + ulist[k+1]**2);
         k += Nu;
 
         for sphere in sphereworld:
-            C += ko*1/sphere.distance(qlist[i]);
+            C += ko*1/sphere.distance(xlist[i]);
 
-    C += kq*((qlist[-1][0] - qd[0])**2 + (qlist[-1][1] - qd[1])**2);
+    C += kx*((xlist[-1][0] - xd[0])**2 + (xlist[-1][1] - xd[1])**2);
 
     return C;
 
-def plot(T, q, params, u=None):
-    sphereworld = params.sphereworld;
-    qd = params.qd;
+def callback(mvar, T, x, u):
+    return mvar.params.update(T, x);
 
-    fig = params.fig;
-    axes = params.axes;
-
-    fig.tight_layout();
-
-    for sphere in sphereworld:
-        sphere.plot(axes);
-
-    if q is not None:
-        qlist = np.transpose(q);
-        axes.plot(qlist[0], qlist[1])
-
-    axes.plot(qd[0], qd[1], color='yellowgreen', marker='*');
-    axes.set_aspect('equal');
-
-def callback(mpc_var, t, q, ulist):
-    axes = mpc_var.params.axes;
-
-    axes.plot(q[0], q[1], color='b', marker='.', markersize=2)
-    plt.show(block=0);
-
-    plt.pause(mpc_var.params.pause);
-
-    return Parameters(mpc_var.params.fig, mpc_var.params.axes);
 
 if __name__ == "__main__":
     num_inputs = 2;
@@ -99,20 +116,19 @@ if __name__ == "__main__":
     num_ssvar = 4;
     model_type = 'discrete';
 
-    params = Parameters();
-    plot(None, None, params)
+    x0 = [-1.68, -9.6, 0, 0];
+    uinit = [0 for i in range(num_inputs*PH_length)];
+
+    params = Parameters(x0, buffer_length=25);
 
     mpc_var = ModelPredictiveControl('nno', model, cost, params, num_inputs,
-        num_ssvar=num_ssvar, PH_length=PH_length, knot_length=4,
+        num_ssvar=num_ssvar, PH_length=PH_length, knot_length=5,
         model_type=model_type);
     mpc_var.setMinTimeStep(1);  # arbitrarily large
 
-    q0 = [-1.68, -9.6, 0, 0];
-    uinit = [0 for i in range(num_inputs*PH_length)];
-
     sim_time = 20;
-    T, qlist, ulist = mpc_var.sim_root(sim_time, q0, uinit,
+    T, xlist, ulist = mpc_var.sim_root(sim_time, x0, uinit,
         callback=callback, output=1)[0:3];
 
-    plot(T, qlist, params);
+    plot(T, xlist, params);
     plt.show();
