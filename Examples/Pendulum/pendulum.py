291a3ff0e212917abd9e5b6ed108eb10088e613e
Michael Napoli <mikednapoli@gmail.com>
2023-07-07 20:51:50 -0400

    pend seems to be working - need to optimize mpc.solve

--- Examples/Pendulum/pendulum.py
+++ Examples/Pendulum/pendulum.py
@@ -22,15 +22,17 @@ def model(x, u):
 
 def cost(x, u):
     xd = np.array( [[np.pi/2],[0]] );
-    kx = 100;  kd = 0;
+    kx = 1000;  kd = 0;
     C = kx*(x[0] - xd[0])**2 + kd*(x[1] - xd[1])**2;
     return C;
 
 def pendulum(tList, xList, uList):
     fig, axs = plt.subplots(2,1);
+
     axs[0].plot(tList.T, xList.T);
+    axs[1].plot(tList.T, uList.T);
+
     axs[0].legend( ('$x_1$', '$x_2$') );
-    axs[1].plot(tList[:,:-1].T, uList.T);
     axs[1].legend( ('$u_1$',) );
 
 if __name__ == "__main__":
@@ -38,25 +40,26 @@ if __name__ == "__main__":
 
     mvar = plant.Model( model, model_type='continuous' );
     mpcvar = opt.ModelPredictiveControl(model, cost,
-        P=3, Nu=1, Nx=2, dt=dt, model_type='continuous');
+        P=10, Nu=1, Nx=2, dt=dt, model_type='continuous');
     mpcvar.setStepSize( 0.1 );
-    mpcvar.setMaxIter( 100 );
+    mpcvar.setMaxIter( 50 );
 
     x0 = np.array( [[0],[-0.1]] );
     uinit = np.zeros( (mpcvar.Nu, mpcvar.P) );
 
-    T = 100;  Nt = round( T/mpcvar.dt ) + 1;
+    T = 25;  Nt = round( T/mpcvar.dt ) + 1;
     tList = np.array( [ [i*mpcvar.dt for i in range( Nt )] ] );
 
-    uList = np.zeros( (mpcvar.Nx, Nt-1) );
+    uList = np.zeros( (mpcvar.Nu*mpcvar.P, Nt) );
     xList = np.empty( (mpcvar.Nx, Nt) );
     xList[:,0] = x0[:,0];
     for i in range( Nt-1 ):
-        uList[:,i] = mpcvar.solve( xList[:,i,None], uinit, verbose=0 )[:,0];
-        xList[:,i+1] = mvar.prop( xList[:,i,None], uList[:,i,None] )[:,0];
+        x = xList[:,i,None];
+        uList[:,i+1] = mpcvar.solve( x, uList[:,i], verbose=0 )[:,0];
+        xList[:,i+1] = mvar.prop( x, uList[:,i+1,None] )[:,0];
         if i % 10 == 0:
             print( 'time =', tList[0][i] );
-            print( uList[:,i] );
+            print( uList[:,i+1] );
 
 
     pendulum( tList, xList, uList[0,None] );
