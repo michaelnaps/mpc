1ded48ddd78e7df18ae25e44bf696b72cd294ba5
Michael Napoli <mikednapoli@gmail.com>
2024-03-10 16:33:36 -0400

    testing sim format

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -11,7 +11,7 @@ MatrixXd model(MatrixXd x, MatrixXd u)
     M << 1, 1, 2,
          2, 1, 0,
          0.5, 1, 0;
-    return M*x;
+    return 0.1*M*x;
 }
 
 int main()
@@ -19,5 +19,15 @@ int main()
     nap::Plant P( model );
     MatrixXd x0( 3, 1 ); x0 << 2, 2, 0.75;
     MatrixXd uX( 3, 1 );
-    cout << P.prop( x0, uX ) << endl;
+
+    // Simulation length.
+    double T = 10;  double dt = 1;
+    int Nt = T/dt;
+
+    MatrixXd x(3,1);  x = x0;
+    for(int i(0); i < Nt; ++i)
+    {
+        x = P.prop( x, uX );
+        cout << x.transpose() << endl;
+    }
 }
