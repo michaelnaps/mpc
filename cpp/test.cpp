4fd6c1443550078b25d38aec0b8ee77ea73ab4e9
Michael Napoli <mikednapoli@gmail.com>
2024-03-09 19:39:13 -0500

    testing function pass

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -4,6 +4,19 @@
 using Eigen::MatrixXd;
 using namespace std;
 
+MatrixXd squared_addition(
+    MatrixXd x,
+    MatrixXd y,
+    MatrixXd (*add)(MatrixXd, MatrixXd))
+{
+    return add(x.transpose()*x, y.transpose()*y);
+}
+
+MatrixXd addition(MatrixXd x, MatrixXd y)
+{
+    return x + y;
+}
+
 int main()
 {
     MatrixXd M(2,2);
@@ -16,11 +29,16 @@ int main()
     M(1,1) = M(1,0) + M(0,1);
 
     // Vector init.
-    x(0,0) = 1;
-    x(1,0) = 2;
+    x(0,0) = 1;  x(1,0) = 2;
 
     // Output.
     cout << M << endl;
     cout << x << endl;
     cout << M*x << endl;
+    cout << "------" << endl;
+
+    // Testing function pass.
+    MatrixXd y(2,1);
+    y(0,0) = 0.1;  y(1,0) = 1;
+    cout << squared_addition(x, y, addition) << endl;
 }
