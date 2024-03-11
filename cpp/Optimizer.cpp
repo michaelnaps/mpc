7eb8670feab3ff04b842198fa05808393432ec3c
Michael Napoli <mikednapoli@gmail.com>
2024-03-10 20:22:42 -0400

    offset and hessian functions need testing/rework

--- cpp/Optimizer.cpp
+++ cpp/Optimizer.cpp
@@ -4,6 +4,19 @@
 namespace nap
 {
     // HELPER FUNCTIONS:
+    // TODO: This is a mess.
+    MatrixXd offset(MatrixXd x, const double &h)
+    {
+        const int N(x.rows()), M(x.cols());
+        MatrixXd y(N,M);
+        for (int i(0); i < N; ++i) {
+            for (int j(0); j < M; ++j) {
+                y(i,j) = x(i,j) + h;
+            }
+        }
+        return y;
+    }
+
     MatrixXd fdm2c(MatrixXd (*obj)(MatrixXd), MatrixXd x, const double &h)
     {
         // State and cost dimensions.
@@ -15,8 +28,8 @@ namespace nap
         MatrixXd xn1(N,1), xp1(N,1);
         MatrixXd gn1(M,1), gp1(M,1);
         for (int i(0); i < N; ++i) {
-            xn1 = x;  xn1(i,0) = xn1(i,0) - h;
-            xp1 = x;  xp1(i,0) = xp1(i,0) + h;
+            xn1 = x;  xn1.row(i) = offset(xn1.row(i), -h);
+            xp1 = x;  xp1.row(i) = offset(xp1.row(i),  h);
 
             gn1 = obj( xn1 );
             gp1 = obj( xp1 );
@@ -43,4 +56,10 @@ namespace nap
     {
         return fdm2c(cost, x, step_size);
     }
+
+    // // TODO: Resolve non-static function error message.
+    // MatrixXd Cost::hessian(MatrixXd x)
+    // {
+    //     return fdm2c(gradient, x, step_size);
+    // }
 }
